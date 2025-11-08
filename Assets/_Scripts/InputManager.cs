81db7c025de6089cc435f3dcf445d563cd5d3580
Jiblly101 <Jiblly101@gmail.com>
2025-11-07 19:42:32 -0500

    working more on gyroscope

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -4,6 +4,19 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    private Vector3 v = Vector3.zero;
+
+    private void Update()
+    {
+        Debug.Log(v);
+    }
+    public void OnGyroscopeTilt(InputAction.CallbackContext context)
+    {
+        Vector3 readDir = context.ReadValue<Vector3>();
+        v = readDir;
+        Debug.Log(readDir);
+        Debug.Log("Beep!");
+    }
     public void OnTilt(InputAction.CallbackContext context)
     {
         Vector2 readDir = context.ReadValue<Vector2>();
